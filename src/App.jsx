f68a0d254a7010f1e5fb8620b9b353b9f2d6f1cb
Tsogt Otgonsuren <ts0gt@Otgonsurens-MacBook-Pro.local>
2023-08-15 22:23:11 +0800

    made it more pretty

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,10 @@ const List = [
   "orange",
   "green",
   "red",
+  "purple",
+  "white",
+  "black",
+  "gray"
 ]
 
 const innitial = "none"
@@ -23,13 +27,26 @@ function App() {
 
     <div className="w-screen h-screen bg-slate-600 flex flex-col items-center">
 
+      <div className='flex flex-row h-[24px] rounded-b-[30px] overflow-clip'>
+        <div className='w-[100px] h-[100%] bg-red-50'></div>
+        <div className='w-[100px] h-[100%] bg-red-100'></div>
+        <div className='w-[100px] h-[100%] bg-red-200'></div>
+        <div className='w-[100px] h-[100%] bg-red-300'></div>
+        <div className='w-[100px] h-[100%] bg-red-400'></div>
+        <div className='w-[100px] h-[100%] bg-red-500'></div>
+        <div className='w-[100px] h-[100%] bg-red-600'></div>
+        <div className='w-[100px] h-[100%] bg-red-700'></div>
+        <div className='w-[100px] h-[100%] bg-red-800'></div>
+
+      </div>
+
       <h1 className='text-white my-14 text-[36px] font-serif'>Welcome to Tsogt's drop down</h1>
 
       <div className='flex flex-row'>
 
-        <Dropdown handleClick={handleClick} dropDownList={List} innitial = {innitial}/>
+        <Dropdown handleClick={handleClick} dropDownList={List} innitial={innitial} />
         <div className='w-24'></div>
-        <Color color={c}/>
+        <Color color={c} />
 
       </div>
 
--- src/components/color.jsx
+++ src/components/color.jsx
@@ -1,21 +1,26 @@
-export const Color = ({color}) =>{
+export const Color = ({ color }) => {
 
-
-    const getTwColor = () =>{
-        switch(color){
+    const getTwColor = () => {
+        switch (color) {
             case "blue":
                 return "bg-blue-500"
-
             case "red":
                 return "bg-red-500"
             case "orange":
                 return "bg-orange-500"
-
             case "green":
                 return "bg-green-500"
-    }
-    
+            case "purple":
+                return "bg-purple-500"
+            case "white":
+                return "bg-white"
+            case "black":
+                return "bg-black"
+            case "gray":
+                return "bg-gray-500"
+        }
+
     }
 
-    return <div className={`w-[300px] h-[300px] ${ color !== "none" ? getTwColor() : '' }`}></div>
+    return <div className={`w-[300px] h-[300px] ${color !== "none" ? getTwColor() : ''}`}></div>
 }
